1832cfb4e302257180d333e12f130bb0ece7b136
kar99k <krthkeyanlee@gmail.com>
2024-07-08 00:40:56 +0530

    metrics added

--- src/components/Atoms/Metrics.jsx
+++ src/components/Atoms/Metrics.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable react/prop-types */
 import { Card, CardFooter, CardHeader } from "@nextui-org/react";
+import NumberTicker from "../magicui/number-ticker";
 
 const Metrics = (props) => {
-
   return (
-    <Card className="w-24 h-[88px] px-5 py-5 gap-1">
-      <CardHeader className="w-full h-1/2 text-4xl font-bold text-lime9 justify-center">
-        {props.header}
+    <Card className="w-full h-[88px] p-2 flex flex-col justify-between items-center">
+      <CardHeader className="mt-2 h-1/2 text-4xl font-bold text-lime9 text-center justify-center tracking-tighter">
+        <NumberTicker value={props.header} stiffVal={100} />
       </CardHeader>
-      <CardFooter className="w-full text-white text-sm font-medium justify-center leading-normal">
+      <CardFooter className="text-white text-sm font-medium text-center justify-center leading-none mb-2">
         {props.footer}
       </CardFooter>
     </Card>
