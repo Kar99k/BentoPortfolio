78c4c5868712d48eab5f6e2019f0b96eec2a45bc
kar99k <krthkeyanlee@gmail.com>
2024-07-28 02:08:02 +0530

    - Preload font

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./assets/font/fontStylesheet.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
