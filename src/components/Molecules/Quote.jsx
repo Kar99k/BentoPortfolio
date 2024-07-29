010a78e206fd23f3ec6cb2b80557b4cd22b9c2bb
kar99k <krthkeyanlee@gmail.com>
2024-07-30 01:43:40 +0530

    - font size change

--- src/components/Molecules/Quote.jsx
+++ src/components/Molecules/Quote.jsx
@@ -5,7 +5,7 @@ import climax from "@/assets/Pics/ClimaxFCx1.webp";
 const Quote = () => {
   return (
     <MyCard className="gap-2 items-center md:w-full sm:w-[390px] md:h-full" color="zinc">
-      <CardBody className="flex text-wrap text-center font-garamond text-[20px] gap-4 leading-tight sm:text-base">
+      <CardBody className="flex text-wrap text-center font-garamond md:text-[22px] gap-4 leading-tight sm:text-base">
         <Image src={climax} sizes={84} />
         <p>
           You met me at a very strange time in my{" "}
