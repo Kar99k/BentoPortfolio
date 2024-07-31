3be3e27016ba283854bf5a349aa935b19a8c25b5
kar99k <krthkeyanlee@gmail.com>
2024-07-31 23:55:24 +0530

    Added CTA

--- src/components/Molecules/CTA.jsx
+++ src/components/Molecules/CTA.jsx
@@ -8,6 +8,13 @@ const CTA = () => {
         color="monoButtonBlue"
         size="monosm"
         startContent={<Icon icon="logos:telegram" width="1em" height="1em" />}
+        onClick={() =>
+          window.open(
+            "https://telegram.me/karkey5994",
+            "_blank",
+            "noopener noreferrer"
+          )
+        }
       >
         Telegram me
       </MyButton>
@@ -17,6 +24,14 @@ const CTA = () => {
         size="monosm"
         startContent={
           <Icon icon="logos:whatsapp-icon" width="1em" height="1em" />
+        }
+        aria-label="Chat on WhatsApp"
+        onClick={() =>
+          window.open(
+            "https://wa.me/919751744041",
+            "_blank",
+            "noopener noreferrer"
+          )
         }
       >
         Whatsapp me
